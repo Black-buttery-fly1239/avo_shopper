dfbdaef4b152dc35c2ef5a348f6396f2064452d2
nobu-hope <nobuhlelebz@gmail.com>
2021-11-23 17:32:47 +0200

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const AvoShopper = require("./avo-shopper");
+
+const pg = require("pg");
+const Pool = pg.Pool;
+
 
 const app = express();
-const PORT =  process.env.PORT || 3019;
+
+const connectionString = process.env.DATABASE_URL || "postgresql://codex:codex123@localhost:5432/my_avo_app";
+
+const pool = new Pool({
+	connectionString,
+	ssl: {
+		rejectUnauthorized: false
+	}
+});
+
 
 // enable the req.body object - to allow us to use HTML forms
 app.use(express.json());
@@ -16,15 +30,42 @@ app.use(express.static('public'));
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
-let counter = 0;
+const avoShopper = AvoShopper(pool);
 
-app.get('/', function(req, res) {
+
+// let counter = 0;
+
+app.get('/', async function(req, res) {
+	const shops = await avoShopper.listShops()
 	res.render('index', {
-		counter
+		shops
 	});
 });
 
+app.get('/shops/shopList', async function (req, res) {
+
+	const shop = await avoShopper.listShops()
+	res.render('shops/shopList', {
+		shop
+	});
+});
+
+app.post('/shops/shopList', async function (req, res) {
+	try {
+		await avoShopper.createShop(req.body.shop_name);
+	} catch (err) {
+		console.log(err)
+	}
+
+
+
+	res.redirect('/shops/shopList')
+});
+
+
+
 // start  the server and start listening for HTTP request on the PORT number specified...
+const PORT =  process.env.PORT || 3019;
 app.listen(PORT, function() {
 	console.log(`AvoApp started on port ${PORT}`)
 });
